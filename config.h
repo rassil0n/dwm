f51b738e6e2aae4ef7829ccabf27da3c8d4eb205
rassil0n <rassil0n@protonmail.com>
2022-07-23 18:25:20 +0200

    add brackets to workspace numbers

--- config.h
+++ config.h
@@ -64,7 +64,7 @@ static Sp scratchpads[] = {
 };
 //}}}
 //{{{ DEFINE TAGS
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "(1)", "(2)", "(3)", "(4)", "(5)", "(6)", "(7)", "(8)", "(9)" };
 //}}}
 //{{{ DEFINE RULES
 static const Rule rules[] = {
